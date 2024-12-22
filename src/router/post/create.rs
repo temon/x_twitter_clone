dac8f66b61814d49a41b725fdd63196ddc3f276a
eko prastyo <ekoamikom+github@gmail.com>
2024-12-22 12:45:49 +0700

    add custom validation

--- src/router/post/create.rs
+++ src/router/post/create.rs
@@ -5,30 +5,35 @@ use axum::{
     async_trait
 };
 use axum::extract::State;
+use axum::http::StatusCode;
 use serde::Deserialize;
 
 #[axum::debug_handler]
 pub async fn create_post(post: CreatePostPayload) {
-    todo!();
 }
 
 // create post payload model
-#[derive(Deserialize, Debug)]
+#[derive(Debug)]
 pub struct CreatePostPayload {
     pub text: String,
     pub parent_id: Option<i32>,
 }
 
+#[derive(Deserialize, Debug)]
+pub struct CreatePostPayloadValidator {
+    pub text: Option<String>,
+}
+
 #[async_trait]
 impl<S> FromRequest<S> for CreatePostPayload
 where
-    Json<Self>: FromRequest<S, Rejection = JsonRejection>,
+    Json<CreatePostPayloadValidator>: FromRequest<S, Rejection = JsonRejection>,
     S: Send + Sync,
 {
     type Rejection = Response;
 
     async fn from_request(req: Request, state: &S) -> Result<Self, Self::Rejection> {
-        let Json(post) = Json::<CreatePostPayload>::from_request(req, state)
+        let Json(post) = Json::<CreatePostPayloadValidator>::from_request(req, state)
             .await
             .map_err(|error| {
                 tracing::error!(
@@ -39,8 +44,25 @@ where
             })?;
 
         tracing::debug!("Parsed request: {:?}", post);
+        
+        let Some(text) = post.text else {
+            return Err((StatusCode::BAD_REQUEST, "text is required").into_response());
+        };
 
-        todo!();
+        // check if it's empty string
+        if text.trim().is_empty() {
+            return Err((StatusCode::BAD_REQUEST, "text cannot be empty").into_response());
+        }
+
+        // check if it's too long (max 255 characters)
+        if text.len() > 255 {
+            return Err((StatusCode::BAD_REQUEST, "text is too long").into_response());
+        }
+
+        Ok(Self {
+            text,
+            parent_id: None,
+        })
     }
 
 }
